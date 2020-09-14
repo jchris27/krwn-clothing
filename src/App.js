642eacc8390f777e3d88eb32f5dcfbf3ca68d472
Jin <jchris1227@icloud.com>
2020-09-14 17:20:52 +0900

    update our menu-items to navigate withRouter Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const Hats = () => {
+  return <div>HATS PAGE</div>;
+};
 function App() {
-  return <HomePage />;
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route path="/shop/hats" component={Hats}></Route>
+    </Switch>
+  );
 }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,14 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem size={size} imageUrl={imageUrl} key={id} title={title} />
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem
+            linkUrl={linkUrl}
+            size={size}
+            imageUrl={imageUrl}
+            key={id}
+            title={title}
+          />
         ))}
       </div>
     );
